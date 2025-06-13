df3aec323856f2e48d2d3a9f69f9cc375c535104
gabriel1049 <gamerfk81@gmail.com>
2025-06-13 16:41:32 -0300

    git

--- wp-content/themes/blog-goat/functions.php
+++ wp-content/themes/blog-goat/functions.php
@@ -6,8 +6,6 @@ function goat_theme_setup() {
     ]);
 }
 add_action('after_setup_theme', 'goat_theme_setup');
-
-
 add_filter('use_block_editor_for_post_type', function($use_block_editor, $post_type) {
     if ($post_type === 'page') {
         return false; // desativa Gutenberg só em páginas
